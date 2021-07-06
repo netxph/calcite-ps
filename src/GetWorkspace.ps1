0ddd41a58add8857ec5f8d47ec673fd454719097
Marc Vitalis <marc.vitalis@live.com>
2021-07-06 15:56:12 +0800

    add set-workspace (#8)
    **Usage**
    
    `Set-Workspace -Name <workspace_name> -Source <source_dir>`
    
    **Examples**
    
    Set to the workspace directory
    
    `Set-Workspace -Name users`
    
    Specify the data source
    
    `Set-Workspace -Name users -Source ./tosh.json`
    
    Related issues: #3

--- src/GetWorkspace.ps1
+++ src/GetWorkspace.ps1
@@ -1,9 +1,6 @@
-. $PSScriptRoot/Privates.ps1
-
 function Get-Workspace(
     [string]$Name,
-    [string]$Source = '~/.config/tosh/tosh.json')
-{
+    [string]$Source = '~/.config/tosh/tosh.json') {
     $data = Get-Content $Source -Raw | ConvertFrom-Json
 
     if($Name -ne '') {
